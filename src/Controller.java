e46df3bce01e5e2b109142245da65c2c862540fb
Vollomat <73344372+Vollomat@users.noreply.github.com>
2022-04-22 14:06:29 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -4,13 +4,12 @@ import java.awt.*;
 public class Controller {
 
     public static final int TOTAL_NUMBER_OF_NODES = 10;
-    public static final Node[] nodes = new Node[TOTAL_NUMBER_OF_NODES];
+    public static Node[] nodes = new Node[TOTAL_NUMBER_OF_NODES];
 
-    public static final int hoehe = 1000; //1m = 10px
-    public static final int breite = 1000;
-    public static final int sendeReichweite = 100;
-    public static Station[] stationen = new Station[Controller.TOTAL_NUMBER_OF_NODES];
-    public static final int schrittgroesse = 10;
+    public static final int HEIGHT = 1000; //1m = 10px
+    public static final int WIDTH = 1000;
+    public static final int RANGE = 100;
+    public static final int STEPSIZE = 10; //Bei Bewegung: wie weit gehen die Nodes?
 
 
     public static void main(String[] args) {
@@ -28,28 +27,19 @@ public class Controller {
     public Controller() {
         JFrame frame = new JFrame("Mobiles Ad-Hoc Netz") {
             public void paint(Graphics g) {
-                for (int i = 0; i < Controller.TOTAL_NUMBER_OF_NODES; i++) {
-                    //int x = (int)(Math.random() * hoehe);
-                    //int y = (int)(Math.random() * breite);
-                    int x = Controller.nodes[i].getX();
-                    int y = Controller.nodes[i].getY();
-                    Graphics2D g2d = (Graphics2D) g;
-                    g2d.drawOval(x, y, sendeReichweite, sendeReichweite);
-                    stationen[i] = Station.punktErstellen(x, y, sendeReichweite);
-                }
                 int j = 20;
                 while (j > 2) {
                     Graphics2D g2d = (Graphics2D) g;
-                    for (int z = 0; z < stationen.length; z++) {
-                        Station neuerStationStandort = stationen[z].bewegung();
-                        g2d.drawOval(stationen[z].getX() - (sendeReichweite / 2), stationen[z].getY() - (sendeReichweite / 2), sendeReichweite, sendeReichweite);
-                        stationen[z] = neuerStationStandort;
+                    for (int z = 0; z < nodes.length; z++) {
+                        Node newNode = nodes[z].bewegung();
+                        g2d.drawOval(nodes[z].getX() - (RANGE / 2), nodes[z].getY() - (RANGE / 2), RANGE, RANGE);
+                        nodes[z] = newNode;
                     }
                     for (int x = 0; x < nodes.length; x++) {
                         for (int y = 0; y < nodes.length; y++) {
-                            if (erreichbarkeit(stationen[x], stationen[y])) {
+                            if (reachable(nodes[x], nodes[y])) {
                                 g2d.setColor(Color.RED);
-                                g2d.drawLine(stationen[x].getX(), stationen[x].getY(), stationen[y].getX(), stationen[y].getY());
+                                g2d.drawLine(nodes[x].getX(), nodes[x].getY(), nodes[y].getX(), nodes[y].getY());
                             }
                         }
                     }
@@ -66,15 +56,16 @@ public class Controller {
             }
         };
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(breite, hoehe);
+        frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
 
 
-    public static boolean erreichbarkeit(Station a, Station b) {
-        return sendeReichweite >= Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2));
+    public static boolean reachable(Node a, Node b) {
+        return RANGE >= Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2));
     }
 
 }
+
