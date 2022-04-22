2cffc2696802a2b87039b725a008fbafdf7126f0
Vollomat <73344372+Vollomat@users.noreply.github.com>
2022-04-22 14:06:52 +0200

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,16 @@
 public class Node extends Thread{
 
-    private int nodeID;
+    private final int nodeID;
     private int x;
     private int y;
-    private int totalNodes;
-    private TCPReceiver receiver;
+    private final int totalNodes;
+    private final TCPReceiver receiver;
     private Thread receiverThread;
-    private TCPSender sender;
+    private final TCPSender sender;
     private Thread senderThread;
-    private static int range = 100;
+    private static final int range = Controller.RANGE;
+
+    public static final int STEPSIZE = Controller.STEPSIZE; //Bei Bewegung: wie weit gehen die Nodes?
 
     public Node(int nodeID, int totalNumberOfNodes){
         this.nodeID = nodeID;
@@ -42,10 +44,130 @@ public class Node extends Thread{
     public int getY(){
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public int getRange(){
         return range;
     }
     public int getNodeID(){
         return nodeID;
+    }
+
+
+    //Stationen zufällig bewegen lassen
+    public Node bewegung() {
+        /*
+        Wahrscheinlichkeiten:
+        ----------------------------
+        Stehen bleiben: 50%         /
+        Links: 7%                   /
+        Rechts: 7%                  /
+        Oben: 7%                    /
+        Unten: 7%                   /
+        Links-Unten: 5.5%           /
+        Links-Oben: 5.5%            /
+        Rechts-Unten: 5.5%          /
+        Rechts-Oben: 5.5%           /
+                                    /
+        Total: 100%                 /
+        ----------------------------
+         */
+
+
+        double zufallszahl = Math.random(); //Diese Zufallszahl soll zufällig entscheiden, wohin sich die Stationen bewegen
+
+        if (zufallszahl >= 0 && zufallszahl < 0.5) {
+            //Zu 50% bewegen sich die Stationen nicht
+        }
+        if (zufallszahl >= 0.5 && zufallszahl < 0.57) {
+            //Zu 7% bewegen sich die Stationen nach links. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() > STEPSIZE) {
+                this.setX(this.getX() - STEPSIZE);
+            }
+        }
+        if (zufallszahl >= 0.57 && zufallszahl < 0.64) {
+            //Zu 7% bewegen sich die Stationen nach rechts. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() + STEPSIZE < Controller.WIDTH) {
+                this.setX(this.getX() + STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.64 && zufallszahl < 0.71) {
+            //Zu 7% bewegen sich die Stationen nach oben. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getY() + STEPSIZE < Controller.HEIGHT) {
+                this.setY(this.getY() + STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.71 && zufallszahl < 0.78) {
+            //Zu 7% bewegen sich die Stationen nach unten. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getY() > STEPSIZE) {
+                this.setY(this.getY() - STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.78 && zufallszahl < 0.835) {
+            //Zu 5.5% bewegen sich die Stationen nach links oben. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() > STEPSIZE) {
+                this.setX(this.getX() - STEPSIZE);
+            } else {
+                this.setX(this.getX() + STEPSIZE);
+            }
+            if (this.getY() > STEPSIZE) {
+                this.setY(this.getY() - STEPSIZE);
+            } else {
+                this.setY(this.getY() + STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.835 && zufallszahl < 0.89) {
+            //Zu 5.5% bewegen sich die Stationen nach links unten. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() > STEPSIZE) {
+                this.setX(this.getX() - STEPSIZE);
+            } else {
+                this.setX(this.getX() + STEPSIZE);
+            }
+            if (this.getY() < Controller.HEIGHT - STEPSIZE) {
+                this.setY(this.getY() + STEPSIZE);
+            } else {
+                this.setY(this.getY() - STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.89 && zufallszahl < 0.945) {
+            //Zu 5.5% bewegen sich die Stationen nach rechts oben. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() < Controller.WIDTH - STEPSIZE) {
+                this.setX(this.getX() + STEPSIZE);
+            } else {
+                this.setX(this.getX() - STEPSIZE);
+            }
+            if (this.getY() > STEPSIZE) {
+                this.setY(this.getY() - STEPSIZE);
+            } else {
+                this.setY(this.getY() + STEPSIZE);
+            }
+        }
+
+        if (zufallszahl >= 0.945 && zufallszahl < 1) {
+            //Zu 5.5% bewegen sich die Stationen nach rechts unten. Voraussetzung: Innerhalb des Spielfelds nach der Bewegung.
+            if (this.getX() < Controller.WIDTH - STEPSIZE) {
+                this.setX(this.getX() + STEPSIZE);
+            } else {
+                this.setX(this.getX() - STEPSIZE);
+            }
+            if (this.getY() < Controller.HEIGHT - STEPSIZE) {
+                this.setY(this.getY() + STEPSIZE);
+            } else {
+                this.setY(this.getY() - STEPSIZE);
+            }
+        }
+        return this;
     }
 }
